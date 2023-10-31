035dae01a1ac2e3c26fccbf9b3dd3cc846b0224e
Nijeesh-bit <njnjfmly@gmail.com>
2023-10-31 11:05:12 +0530

    first commit

--- dataproject/myapp/models.py
+++ dataproject/myapp/models.py
@@ -1,26 +1,3 @@
-# Django ORM Web Application
-
-## AIM
-To develop a Django application to store and retrieve data from a database using Object Relational Mapping(ORM).
-
-## Entity Relationship Diagram
-
-Include your ER diagram here
-
-## DESIGN STEPS
-
-### STEP 1:
-
-### STEP 2:
-
-### STEP 3:
-
-Write your own steps
-
-## PROGRAM
-
-models.py
-```
 from django.db import models
 from django.contrib import admin
 
@@ -42,20 +19,3 @@ class Employee (models.Model):
 
 class EmployeeAdmin(admin.ModelAdmin):
     list_display=('emp_id','ename','post','salary')
-```
-admin.py
-```
-from django.contrib import admin
-from .models import Student,StudentAdmin,Employee,EmployeeAdmin
-# Register your models here.
-admin.site.register(Student,StudentAdmin)
-admin.site.register(Employee,EmployeeAdmin)
-
-```
-
-## OUTPUT
-
-Include the screenshot of your admin page.
-![Alt text](<Screenshot from 2023-10-31 10-40-19.png>)
-
-## RESULT
